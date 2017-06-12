bcaba6f7181b1d12df687ad4f151676ba10e79d0
Pascal Corpet <pascal@bayesimpact.org>
2017-06-12 16:58:27 +0200

    Fix indentation and switch to eslint v4.

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ var config = require('./webpack.config')
 const host = process.env.BIND_HOST || 'localhost'
 
 new WebpackDevServer(webpack(config), config.devServer).
-listen(config.devServer.port, host, function(err) {
-  if (err) {
-    console.log(err)
-  }
-  console.log('Listening at ' + host + ':' + config.devServer.port)
-})
+  listen(config.devServer.port, host, function(err) {
+    if (err) {
+      console.log(err)
+    }
+    console.log('Listening at ' + host + ':' + config.devServer.port)
+  })
