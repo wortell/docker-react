0e09843b288973c2fcdb54beb9c246ccae089122
Cyrille Corpet <cyrille@bayesimpact.org>
2020-09-10 18:34:37 +0200

    Allow custom job name for dependant repo. (#2390)

--- .circleci/test_other_repo.sh
+++ .circleci/test_other_repo.sh
@@ -2,29 +2,30 @@
 # TODO(cyrille): Add doc.
 set -e
 
-readonly project=$1
+readonly PROJECT=$1
+readonly JOB_NAME=${2:-"test-for-base-change"}
 readonly CIRCLE_FOLDER="$(dirname ${BASH_SOURCE[0]})"
 readonly TAG="$(bash $CIRCLE_FOLDER/tag.sh)"
 # TODO(cyrille): Use a workflow build once they allow adding build parameters.
 readonly BUILD_NUM=$(curl -s -u $CIRCLE_API_KEY: \
-  -d "build_parameters[REACT_BASE_TAG]=$TAG&build_parameters[CIRCLE_JOB]=test-for-base-change" \
-  "https://circleci.com/api/v1.1/project/github/$project/tree/master" |
+  -d "build_parameters[REACT_BASE_TAG]=$TAG&build_parameters[CIRCLE_JOB]=$JOB_NAME" \
+  "https://circleci.com/api/v1.1/project/github/$PROJECT/tree/master" |
   jq -r '.build_num')
-readonly BUILD_URL="https://circleci.com/gh/$project/$BUILD_NUM"
+readonly BUILD_URL="https://circleci.com/gh/$PROJECT/$BUILD_NUM"
 
 outcome=null
-echo -n "Waiting on external build in $project ($BUILD_URL)."
+echo -n "Waiting on external build in $PROJECT ($BUILD_URL)."
 while [[ $outcome == null ]]; do
   sleep 60
   outcome="$(curl -s -u $CIRCLE_API_KEY: \
-    "https://circleci.com/api/v1.1/project/github/$project/$BUILD_NUM" |
+    "https://circleci.com/api/v1.1/project/github/$PROJECT/$BUILD_NUM" |
       jq -r '.outcome')"
   echo -n "."
 done
 echo ""
 
 if [[ "$outcome" != success ]]; then
-  echo "Building $project with tag $TAG got outcome $outcome."
+  echo "Building $PROJECT with tag $TAG got outcome $outcome."
   echo "More info at $BUILD_URL"
   exit 1
 fi
